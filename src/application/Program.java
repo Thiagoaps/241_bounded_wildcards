dc2314d85029f62ae43e0ffe8da98a86b6bc10f9
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-19 18:46:31 -0300

    Bounded Wildcards class02

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Program {
 	
 	public static void main(String[] args) {
-		
+		    
 		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
 		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
